e7cdbea9e20e12fc3049a0ad2efccbc43f77b1aa
AndyLiu <zero.andy@msa.hinet.net>
2019-04-11 03:52:45 +0800

    first commit

---
+++ PluginDependency/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PluginDependency
+{
+    // This is a simple class to write message to console. It is present to demostrate
+    // how a dependency of the plugin gets loaded into the HostAssemblyLoadContext
+    public class Logger
+    {
+        public static void LogMessage(string msg)
+        {
+            Oracle.ManagedDataAccess.Client.OracleConnection db = new Oracle.ManagedDataAccess.Client.OracleConnection();
+            db.Dispose();
+            db = null;
+            Console.WriteLine(msg);
+        }
+    }
+    class dog
+    {
+        public string Name { get; set; }
+        public int years { get; set; }
+    }
+}
